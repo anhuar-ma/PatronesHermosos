36a6e8a095ed8e37d070706a82cb9cb63f343283
Javierch1405 <javierch140504@outlook.com>
2025-05-15 13:50:14 -0600

    useCurrentRol, barraAdminSede, RegistroMentoraCoordinadora

---
+++ client/src/components/AdminNavBarSede.jsx
@@ -0,0 +1,116 @@
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import BurguerButton from "./BurguerButton"; // Asegúrate de importar el mismo
+import {
+  House,
+  Users,
+  User,
+  BookMarked,
+  ChevronRight,
+  Contact,
+  LogOut,
+  Compass,
+} from "lucide-react";
+import "../styles/Sidebar.css"; // Asegúrate de importar los estilos
+
+export default function AdminNavbar() {
+  const [active, setActive] = useState("/");
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate(); // Add navigate hook
+  const { logout } = useAuth(); // Get logout function from auth context
+
+
+  useEffect(() => {
+    setMenuOpen(false); // Cierra menú al navegar
+  }, [location.pathname]);
+
+  const handleLogout = () =>{
+    logout();
+    navigate("/");
+
+  }
+
+  const navItems = [
+    {
+      name: "Inicio",
+      icon: <House size={20} className="AdminNavBar__icon" />,
+      path: "/admin/inicio",
+    },
+    {
+      name: "Grupos",
+      icon: <Users size={20} className="AdminNavBar__icon" />,
+      path: "/admin/grupos",
+    },
+    {
+      name: "Participantes",
+      icon: <User size={20} className="AdminNavBar__icon " />,
+      path: "/admin/participantes",
+    },
+    {
+      name: "Mentoras",
+      icon: <Compass size={20} className="AdminNavBar__icon " />,
+      path: "/admin/mentoras",
+    },
+    {
+      name: "Colaboradores",
+      icon: <Contact size={20} className="AdminNavBar__icon" />,
+      path: "/admin/colaboradores",
+    },
+    {
+      name: "Diplomas",
+      icon: <BookMarked size={20} className="AdminNavBar__icon " />,
+      path: "/admin/diplomas",
+    },
+  ];
+
+  return (
+    <div className="AdminNavbar">
+      <div className="AdminNavbar__title">
+        <img
+          src="../src/assets/logo_patrones.png"
+          alt="hola"
+          className="AdminNavbar__imgLogoNav"
+        />
+        <p>Patrones Hermosos</p>
+      </div>
+      <div className={`AdminNavbar__nav ${menuOpen ? "active" : ""}`}>
+        {navItems.map((item) => (
+          <Link
+            to={item.path}
+            key={item.path}
+            onClick={() => setActive(item.path)}
+            className={`AdminNavbar__button ${
+              active === item.path ? "active" : ""
+            }`}
+          >
+            <div className="AdminNavbar__container-icon-name">
+              {item.icon}
+              <span className="AdminNavbar__buttonLabel">{item.name}</span>
+            </div>
+            <ChevronRight size={20} />
+          </Link>
+        ))}
+
+          {/* <div className="AdminNavbar__footer"> */}
+      <button
+        onClick={handleLogout}
+        className="AdminNavbar__button-logout"
+      >
+        <LogOut size={20} />
+        <span className="AdminNavbar__buttonLabel">Cerrar sesión</span>
+      </button>
+        {/* </div> */}
+      </div>
+
+      <div
+        className="AdminNavBar__burguerButton"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <BurguerButton open={menuOpen} />
+      </div>
+    </div>
+  );
+}
---
+++ client/src/pages/admin/RegistroMentora.jsx
@@ -0,0 +1,143 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "../../styles/registros.css/";
+import axios from "axios";
+
+export default function RegistroSedes() {
+  const [error, setError] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  
+
+  const onSubmit = async (data) => {
+
+    try {
+      const sedeData = {
+        // Coordinatora
+        nombre_coordinadora: data.nombre_coordinadora,
+        apellido_paterno_coordinadora: data.apellido_paterno_coordinadora,
+        apellido_materno_coordinadora: data.apellido_materno_coordinadora,
+        correo_coordinadora: data.correo_coordinadora,
+        contraseña: data.contraseña,
+
+        // Sede
+        nombre_sede: data.nombre_sede,
+        fecha_inicio: data.fecha_inicio,
+        archivo_convocatoria: fileName, // The file name, you might need to handle the actual file upload separately
+      };
+
+      await axios.post("/api/sedes", sedeData);
+
+      setError("");
+    } catch (error) {
+      window.alert("Error en el registro");
+      console.error("Error:", error);
+    }
+
+    setError("");
+    alert("Formulario enviado correctamente ✅");
+    console.log(data);
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      if (file.type !== "application/pdf") {
+        setFileError("Solo se permiten archivos PDF");
+        setFileName("");
+        return;
+      }
+      setFileName(file.name);
+      setFileError("");
+    }
+  };
+
+  return (
+    <div className="registroMentora__fondoMorado">
+      <div className="register-container-mentora">
+      <div className="registro_containerForm">
+      <h2 className="registro__titulo">Registro de mentora</h2>
+        <form
+          className="registro__formulario"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <h4 className="registro__tituloSeccion">Información personal</h4>
+            <p className="registro__instrucciones">
+              Llenar con datos como aparecen en un documento oficial.
+            </p>
+            <label>
+              Nombre(s) de la mentora <span className="registro__obligatorio">*</span>
+              <br />
+              <input
+                className={`registro__input ${
+                  errors.nombre_mentora ? "registro__input-error" : ""
+                }`}
+                {...register("nombre_mentora", { required: true })}
+                type="text"
+              />
+              {errors.nombre_mentora && (
+                <p className="registro__error">Este campo es obligatorio</p>
+              )}
+            </label>
+
+            <label>
+              Apellido paterno de la mentora{" "}
+              <span className="registro__obligatorio">*</span>
+              <br />
+              <input
+                className={`registro__input ${
+                  errors.apellido_paterno_mentora ? "registro__input-error" : ""
+                }`}
+                {...register("apellido_paterno_mentora", {
+                  required: true,
+                })}
+                type="text"
+              />
+              {errors.apellido_paterno_mentora && (
+                <p className="registro__error">Este campo es obligatorio</p>
+              )}
+            </label>
+
+            <label>
+              Apellido materno de la mentora
+              <br />
+              <input
+                className="registro__input"
+                {...register("apellido_materno_coordinadora")}
+                type="text"
+              />
+            </label>
+
+            <label>
+              Correo <span className="registro__obligatorio">*</span>
+              <br />
+              <input
+                className={`registro__input ${
+                  errors.correo_mentora ? "registro__input-error" : ""
+                }`}
+                {...register("correo_mentora", { required: true })}
+                type="email"
+              />
+              {errors.correo_mentora && (
+                <p className="registro__error">Correo inválido o vacío</p>
+              )}
+            </label>
+
+          <div className="registro__contenedor__botonSubmit">
+            <input
+              type="submit"
+              className="registro__botonMorado"
+              value="Registrar mentora"
+            />
+          </div>
+        </form>
+        </div>
+      </div>
+    </div>
+  );
+}
